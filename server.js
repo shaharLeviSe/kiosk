42401c17c4b09e64a0cb32ad2ff0cb217e6ef9cc
shaharLeviSe <shahar.levi.se@gmail.com>
2016-10-12 22:41:10 +0300

    fix the use of objects in server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.use(bodyParser.json()); // to support JSON-encoded bodies
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.static(__dirname + '/static'));
 
-var kioskService = new Service(server);
+var kioskService = new Service.Service(server);
 kioskService.register();
 
 console.log("starting server on port 3000..");
